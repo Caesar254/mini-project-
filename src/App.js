6dd5bce3749e990b330e4e6f11d2e220c31bfed2
Caesar254 <augustineodu9@gmail.com>
2023-11-24 16:47:11 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter ,Routes, Route } from "react-router-dom";
+//import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Nav from "./Nav";
+import Posts from "./Posts";
+import Footer from "./Footer";
+import Login from "./Login";
+import Loggged from "./Logged"
 
-function App() {
+
+
+
+
+function App()  {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+      <Nav/>
+     
+      
+        <Routes>
+        
+        <Route exact path="/" element={<Posts />}></Route>
+        {/* <Route path='/login' element={<Login/>}></Route> */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/logged" element={<Loggged />} />
+       
+        
+        
+          </Routes> 
+      
+     <Footer/>
+      
+      
+      </BrowserRouter>
+          
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
